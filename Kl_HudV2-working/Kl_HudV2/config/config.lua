7d7c1460f364c87ffc331487ae3c649745fe02a4
Romeo <38995227+RomeVladoHQ@users.noreply.github.com>
2021-05-13 21:47:29 -0300

    Add files via upload

---
+++ Kl_HudV2-working/Kl_HudV2/config/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+Config['TickTime'] = 1000
+Config['Hud'] = true
+Config['HideMinimap'] = false
+Config['Stress'] = false
+Config['PlayerID'] = false
+Config['Fuel'] = true --only Visible in Car
